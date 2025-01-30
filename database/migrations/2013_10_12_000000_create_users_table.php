e8a884e2508f825fcb704062437847a4d19d2d96
KulikovAR <106818844+SashkaBackEnd@users.noreply.github.com>
2025-01-30 18:08:41 +0300

    fix migrations

--- database/migrations/2013_10_12_000000_create_users_table.php
+++ database/migrations/2013_10_12_000000_create_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->string('name')->nullable();
-            $table->string('phone')->unique();
+            $table->id('id');
+            $table->string('name');
+            $table->string('phone')->unique()->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('code')->nullable();
             $table->timestamp('code_send_at')->nullable();
